5e5f3b83eebd1540db04c9c35fad7e2f84dafdbb
Saruwein <123252575+Saruwein@users.noreply.github.com>
2024-10-03 16:25:20 +0200

    Commit changes after fixing line endings and ignoring temp files
    Former-commit-id: 1ccf999e6db044f91e8745e405ef39ee6dc74101
    Former-commit-id: cd1fff9deb323aac297e2b17da85d6d6fe5257b0

--- Assets/Scripts/Collection.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public static class Collection
-{
-    public static bool collect_AFrame = false;
-    public static bool collect_Money = false;
-    public static bool collect_Jigsaw = false;
-}
